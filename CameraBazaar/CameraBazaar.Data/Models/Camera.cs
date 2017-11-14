4a1bf236f3d63e45133c51eb774f8e1d2d45c87e
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-11-14 15:29:17 +0200

    CarBazaar Initial

---
+++ CameraBazaar/CameraBazaar.Data/Models/Camera.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace CameraBazaar.Data.Models
+{
+    public class Camera
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Range(0, 100)]
+        public int Quantity { get; set; }
+
+        [Range(1, 30)]
+        public int MinShutterSpeed  { get; set; }
+
+        [Range(2000, 8000)]
+        public int MaxShutterSpeed { get; set; }
+
+        [Range(50, 100)]
+        public int MinIso { get; set; }
+
+        [Range(200, 409600)]
+        public int MaxIso { get; set; }
+
+        [MaxLength(15)]
+        public bool IsFullFrame { get; set; }
+
+        public string VideoResolution { get; set; }
+
+        public string LightMetering  { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
