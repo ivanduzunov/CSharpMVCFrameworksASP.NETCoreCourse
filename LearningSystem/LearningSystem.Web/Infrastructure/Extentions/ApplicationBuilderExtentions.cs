40c70c758310da96c21b591c595fbabeff70992f
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-11-22 18:07:54 +0200

    LearningSysstem refactored

---
+++ LearningSystem/LearningSystem.Web/Infrastructure/Extentions/ApplicationBuilderExtentions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningSystem.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Builder;
+
+namespace LearningSystem.Web.Infrastructure.Extentions
+{
+    public static class ApplicationBuilderExtentions
+    {
+        public static IApplicationBuilder UseDatabaseMigration(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<LearningSystemDbContext>()
+                .Database.Migrate();               
+            }
+            return app;
+        }
+    }
+}
---
+++ LearningSystem/LearningSystem.Web/Infrastructure/Mapping/IHaveCustomMapping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace LearningSystem.Web.Infrastructure.Mapping
+{
+    public interface IHaveCustomMapping
+    {
+        void ConfigureMapping(Profile mapper);
+    }
+}
