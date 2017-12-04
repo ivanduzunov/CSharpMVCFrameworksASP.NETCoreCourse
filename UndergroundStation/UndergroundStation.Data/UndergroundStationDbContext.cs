304958caa665dd144ff0b722de455bf0d6c6d254
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-12-04 19:06:07 +0200

    UndergroundStation project initial commit. Completed: AutoMapper, Initial Migration To do: Database models

---
+++ UndergroundStation/UndergroundStation.Data/UndergroundStationDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace UndergroundStation.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class UndergroundStationDbContext : IdentityDbContext<User>
+    {
+        public UndergroundStationDbContext(DbContextOptions<UndergroundStationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ UndergroundStation/UndergroundStation.Web/Infrastructure/WebConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UndergroundStation.Web.Infrastructure
+{
+    public class WebConstants
+    {
+        public const string AdministratorRole = "Administrator";
+        public const string AuthorRole = "Author";
+        public const string PublisherRole = "Publisher";
+
+        public const string TempDataSuccessMessageKey = "SuccessMessage";
+        public const string TempDataErrorMessageKey = "ErrorMessage";
+
+        public const string AdminArea = "Admin";
+        public const string AuthorArea = "Author";
+    }
+}
